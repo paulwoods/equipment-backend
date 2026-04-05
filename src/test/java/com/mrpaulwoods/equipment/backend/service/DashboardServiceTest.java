3bd1ec920462b88581c7f1e0ae0aacc1b8859176
Paul Woods <mr.paul.woods@gmail.com>
2026-04-04 21:22:16 -0500

    chore: add backend module to project and suppress warning in DashboardServiceTest
    - Included backend module in the IntelliJ project configuration.
    - Suppressed "SequencedCollectionMethodCanBeUsed" warning in `DashboardServiceTest` to ensure cleaner code.
    
    Signed-off-by: Paul Woods <mr.paul.woods@gmail.com>

--- src/test/java/com/mrpaulwoods/equipment/backend/service/DashboardServiceTest.java
+++ src/test/java/com/mrpaulwoods/equipment/backend/service/DashboardServiceTest.java
@@ -21,6 +21,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("SequencedCollectionMethodCanBeUsed")
 @ExtendWith(MockitoExtension.class)
 class DashboardServiceTest {
 
