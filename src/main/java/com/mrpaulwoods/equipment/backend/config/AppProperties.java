261574f248ca73007a06810773027100169f12a0
Paul Woods <mr.paul.woods@gmail.com>
2026-04-12 20:48:52 -0500

    feat: enhance configuration and deployment setup
    - Added `APP_URL` to `application.yaml` and `.env.example` for better app configuration.
    - Commented out SMTP user and password fields in configuration files.
    - Introduced `restart.sh` script to automate Docker deployment process.
    - Updated `.env.example` for deployment environment to reflect new and consistent settings.
    
    Signed-off-by: Paul Woods <mr.paul.woods@gmail.com>

--- src/main/java/com/mrpaulwoods/equipment/backend/config/AppProperties.java
+++ src/main/java/com/mrpaulwoods/equipment/backend/config/AppProperties.java
@@ -8,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class AppProperties {
     private String jwtSecret = "changeme-use-a-32-char-secret-here!!!!";
     private long jwtExpirationMs = 3_600_000L;
-    private String smtpUser;
-    private String smtpPass;
+    //    private String smtpUser;
+//    private String smtpPass;
     private String smtpFrom;
     private String appUrl = "http://localhost:8080";
     private String emailRecipient;
